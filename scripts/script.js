1edf78938b0bd102c4c8f1e14e7c74964049d7dd
tstokes22 <tbstokes22@gmail.com>
2024-11-13 09:32:25 -0500

    modified:   index.html 	modified:   scripts/script.js 	modified:   styles/styles.css

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 class ToDoList {
     constructor() {
-      this.taskInput = document.getElementById("taskInput");
+      this.taskInput = document.getElementById("input");
       this.taskList = document.getElementById("taskList");
       this.bindEventListeners();
       this.addTask();
@@ -23,27 +23,38 @@ class ToDoList {
     addTask() {
       const taskName = this.taskInput.value.trim();
       if (taskName !== "") {
+    
         const task = document.createElement("li");
         task.setAttribute("id", "task");
+
+        this.taskList.appendChild(task);
+        this.taskInput.value = "";
+        this.checkInput();     
+
+        //Checkbox for task
         const taskCheckBox = document.createElement("input");
         taskCheckBox.setAttribute("id", "taskCheckBox");
+        taskCheckBox.type = "checkbox";
+        
+
+        //Set inner text of task element to the input value
+        task.textContent = taskName;
+
+        //Delete Task button
         const removeButton = document.createElement("input");
         removeButton.type = "image";
         removeButton.src = "images/trash.svg";
         removeButton.setAttribute("id", "removeTaskButton");
-       
-        taskCheckBox.type = "checkbox";
-        task.innerText = taskName;
-        this.taskList.append(removeButton);
-        this.taskList.append(taskCheckBox);
-        this.taskList.append(task);
-        this.taskInput.value = "";
-        this.checkInput();
+        
+        task.appendChild(taskCheckBox);
+        task.appendChild(removeButton);
+        
         removeButton.addEventListener('click', () =>{
           task.remove();
           removeButton.remove();
           taskCheckBox.remove();
         });
+        
 
         taskCheckBox.addEventListener('change', () => {
           if(taskCheckBox.checked){
